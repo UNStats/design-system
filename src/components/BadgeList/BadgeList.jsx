b87a08f951260492a66fd717d498e8411ac24059
Thilo Maier <git@maier.asia>
2018-07-16 16:25:32 -0400

    Rename render prop to anchor in BadgeList

--- src/components/BadgeList/BadgeList.jsx
+++ src/components/BadgeList/BadgeList.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { space } from "styled-system";
-import theme from "../theme";
 import Badge from "../Badge";
 
 const List = styled.div`
+  div:last-child {
+    margin-right: 0;
+  }
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   ${space};
 `;
 
-const BadgeList = ({ badges, render, ...props }) => (
+const BadgeList = ({ anchor, values, ...props }) => (
   <List {...props}>
-    {badges.map(({ color, href, text }) => (
-      <Badge color={color} href={href} render={render} p={1} key={text}>
+    {values.map(({ color, href, text }, index) => (
+      <Badge
+        anchor={anchor}
+        color={color}
+        href={href}
+        mb={2}
+        mr={2}
+        key={index}
+      >
         {text}
       </Badge>
     ))}
@@ -23,18 +32,12 @@ const BadgeList = ({ badges, render, ...props }) => (
 );
 
 BadgeList.propTypes = {
-  badges: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      href: PropTypes.string.isRequired,
-      color: PropTypes.oneOf(Object.keys(theme.colors))
-    })
-  ).isRequired,
-  render: PropTypes.func
+  anchor: PropTypes.func,
+  values: PropTypes.arrayOf(Badge.propTypes).isRequired
 };
 
 BadgeList.defaultProps = {
-  render: (href, text) => <a href={href}>{text}</a>
+  anchor: (href, text) => <a href={href}>{text}</a>
 };
 
 export default BadgeList;
--- src/components/BadgeList/BadgeList.stories.jsx
+++ src/components/BadgeList/BadgeList.stories.jsx
@@ -2,68 +2,28 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { NavLink, MemoryRouter } from "react-router-dom";
 import BadgeList from "./BadgeList";
+import Box from "../Box";
 
-// Badges without href. href needs to be filled in by each story.
 const badges = [
-  { text: "Committee Member", color: "red" },
-  { text: "Organizer", color: "green" },
-  { text: "Keynote Speaker", color: "yellow" },
-  { text: "Speaker" }
+  { text: "Committee Member", href: "#", color: "red" },
+  { text: "Organizer", href: "#", color: "green" },
+  { text: "Keynote Speaker", href: "#", color: "yellow" },
+  { text: "Speaker", href: "#" }
 ];
 
-// Custom render prop.
-const render = (href, text) => <NavLink to={href}>{text}</NavLink>;
-
 storiesOf("BadgeList", module)
-  .add("default render prop", () => (
-    <BadgeList
-      badges={badges.map(badge => ({
-        ...badge,
-        href: "/?selectedKind=BadgeList&selectedStory=default%20render%20prop"
-      }))}
-      p={1}
-    />
-  ))
-  .add("custom render prop", () => (
+  .addDecorator(story => <Box p={1}>{story()}</Box>)
+  .add("default anchor", () => <BadgeList values={badges} />)
+  .add("custom anchor", () => (
     <MemoryRouter>
       <BadgeList
-        badges={badges.map(badge => ({
-          ...badge,
-          href: "/?selectedKind=BadgeList&selectedStory=custom%20render%20prop"
-        }))}
-        render={render}
-        p={1}
+        anchor={(href, text) => <NavLink to={href}>{text}</NavLink>}
+        values={badges}
       />
     </MemoryRouter>
   ))
   .add("many badges", () => (
     <BadgeList
-      badges={[
-        { text: "Badge 01" },
-        { text: "Badge 02", color: "blue" },
-        { text: "Badge 03", color: "red" },
-        { text: "Badge 04", color: "green" },
-        { text: "Badge 05", color: "yellow" },
-        { text: "Badge 06", color: "gray" },
-        { text: "Badge 07", color: "black" },
-        { text: "Badge 08", color: "primary" },
-        { text: "Badge 09", color: "blue" },
-        { text: "Badge 10", color: "red" },
-        { text: "Badge 11", color: "green" },
-        { text: "Badge 12", color: "yellow" },
-        { text: "Badge 13", color: "gray" },
-        { text: "Badge 14", color: "black" },
-        { text: "Badge 15" },
-        { text: "Badge 16", color: "blue" },
-        { text: "Badge 17", color: "red" },
-        { text: "Badge 18", color: "green" },
-        { text: "Badge 19", color: "yellow" },
-        { text: "Badge 20", color: "gray" },
-        { text: "Badge 21", color: "black" }
-      ].map(badge => ({
-        ...badge,
-        href: "/?selectedKind=BadgeList&selectedStory=many%20badges"
-      }))}
-      p={1}
+      values={[...badges, ...badges, ...badges, ...badges, ...badges]}
     />
   ));
