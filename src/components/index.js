1892bad531e82a66c950d82f81d2da132aed171f
Thilo Maier <git@maier.asia>
2018-08-07 15:14:56 -0400

    Remove Box and Container from exports

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import AlternateTitle from "./AlternateTitle";
+import DefaultTitle from "./DefaultTitle";
+import Header from "./Header";
+import List from "./List";
+import ProfileHeader from "./ProfileHeader";
+import ProfilePreview from "./ProfilePreview";
+import Provider from "./Provider";
+
+export {
+  AlternateTitle,
+  DefaultTitle,
+  Header,
+  List,
+  ProfileHeader,
+  ProfilePreview,
+  Provider
+};
--- src/index.js
+++ src/index.js
@@ -1,25 +1,2 @@
-import AlternateTitle from "./components/AlternateTitle";
-import Box from "./components/Box";
-import DefaultTitle from "./components/DefaultTitle";
-import Container from "./components/Container";
-import Header from "./components/Header";
-import List from "./components/List";
-import MarkdownAnchor from "./components/MarkdownAnchor";
-import MarkdownParagraph from "./components/MarkdownParagraph";
-import ProfileHeader from "./components/ProfileHeader";
-import ProfilePreview from "./components/ProfilePreview";
-import Provider from "./components/Provider";
-
-export {
-  AlternateTitle,
-  Box,
-  Container,
-  DefaultTitle,
-  Header,
-  List,
-  MarkdownAnchor,
-  MarkdownParagraph,
-  ProfileHeader,
-  ProfilePreview,
-  Provider
-};
+export * from "./components";
+export * from "./markdown";
