f062cd71583ade93a3f50a859d7e7b15e0591a5e
Thilo Maier <git@maier.asia>
2018-08-13 15:15:26 -0400

    Remove link from ProfilePreview

--- src/components/ProfilePreview/ProfilePreview.jsx
+++ src/components/ProfilePreview/ProfilePreview.jsx
@@ -1,31 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import {
   alignItems,
-  borders,
-  borderColor,
   flexDirection,
   fontSize,
   space,
   textAlign
 } from "styled-system";
-import PropTypes from "prop-types";
-import Avatar from "../Avatar";
-import { anchorStyle } from "../../style";
+import Avatar from "../../helpers/Avatar";
 
 const Wrapper = styled.article`
-  ${anchorStyle};
-  ${borders};
-  ${borderColor};
-  ${space};
-  &:last-child {
-    border: 0;
-    margin-bottom: 0;
-    padding-bottom: 0;
-  }
-`;
-
-const Preview = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -58,59 +43,28 @@ const Organization = styled.h2`
   ${textAlign};
 `;
 
-const ProfilePreview = ({
-  anchor,
-  color,
-  href,
-  img,
-  jobtitle,
-  name,
-  organization,
-  ...props
-}) => (
-  <Wrapper borderColor={color} borderBottom="2px solid" {...props}>
-    {anchor({
-      href,
-      children: (
-        <Preview flexDirection={["column", "row"]} pb={3}>
-          <Avatar
-            alt={name}
-            img={img}
-            size={["md", "md", "lg"]}
-            mb={2}
-            mr={[0, 3]}
-          />
-          <Description alignItems={["center", "flex-start"]}>
-            <Name fontSize={3} textAlign={["center", "left"]} mt={0} mb={2}>
-              {name}
-            </Name>
-            {jobtitle && (
-              <Jobtitle
-                fontSize={5}
-                textAlign={["center", "left"]}
-                mt={0}
-                mb={1}
-              >
-                {jobtitle}
-              </Jobtitle>
-            )}
-            {organization && (
-              <Organization fontSize={5} textAlign={["center", "left"]} my={0}>
-                {organization}
-              </Organization>
-            )}
-          </Description>
-        </Preview>
-      )
-    })}
+const ProfilePreview = ({ img, jobtitle, name, organization }) => (
+  <Wrapper flexDirection={["column", "row"]} pb={3}>
+    <Avatar alt={name} img={img} size={["md", "md", "lg"]} mb={2} mr={[0, 3]} />
+    <Description alignItems={["center", "flex-start"]}>
+      <Name fontSize={3} textAlign={["center", "left"]} mt={0} mb={2}>
+        {name}
+      </Name>
+      {jobtitle && (
+        <Jobtitle fontSize={5} textAlign={["center", "left"]} mt={0} mb={1}>
+          {jobtitle}
+        </Jobtitle>
+      )}
+      {organization && (
+        <Organization fontSize={5} textAlign={["center", "left"]} my={0}>
+          {organization}
+        </Organization>
+      )}
+    </Description>
   </Wrapper>
 );
 
 ProfilePreview.propTypes = {
-  /** Render prop for anchors. */
-  anchor: PropTypes.func,
-  color: PropTypes.string,
-  href: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
   jobtitle: PropTypes.string,
   name: PropTypes.string.isRequired,
@@ -118,9 +72,6 @@ ProfilePreview.propTypes = {
 };
 
 ProfilePreview.defaultProps = {
-  // eslint-disable-next-line react/prop-types
-  anchor: ({ href, children }) => <a href={href}>{children}</a>,
-  color: "primary",
   jobtitle: "",
   organization: ""
 };
--- src/components/ProfilePreview/ProfilePreview.stories.jsx
+++ src/components/ProfilePreview/ProfilePreview.stories.jsx
@@ -1,38 +1,20 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { NavLink, MemoryRouter } from "react-router-dom";
 import ProfilePreview from "./ProfilePreview";
-import profiles from "../../profiles";
+import rawProfiles from "../../profiles";
+
+const profiles = rawProfiles.map(({ href, ...profile }) => ({ ...profile }));
 
 storiesOf("components/ProfilePreview", module)
   .add("with jobtitle and organization", () => (
     <ProfilePreview {...profiles[0]} />
   ))
-  .add("multiple siblings", () => (
-    <React.Fragment>
-      <ProfilePreview {...profiles[1]} pb={3} mb={3} />
-      <ProfilePreview {...profiles[2]} pb={3} mb={3} />
-    </React.Fragment>
-  ))
+  .add("really long strings", () => <ProfilePreview {...profiles[1]} />)
   .add("no jobtitle", () => {
-    const { jobtitle, ...profile } = profiles[3];
+    const { jobtitle, ...profile } = profiles[2];
     return <ProfilePreview {...profile} />;
   })
   .add("no organization", () => {
-    const { organization, ...profile } = profiles[4];
+    const { organization, ...profile } = profiles[3];
     return <ProfilePreview {...profile} />;
-  })
-  .add("custom color", () => (
-    <React.Fragment>
-      <ProfilePreview {...profiles[5]} pb={3} mb={3} color="green" />
-      <ProfilePreview {...profiles[6]} pb={3} mb={3} color="green" />
-    </React.Fragment>
-  ))
-  .add("custom anchor", () => (
-    <MemoryRouter>
-      <ProfilePreview
-        anchor={({ href, children }) => <NavLink to={href}>{children}</NavLink>}
-        {...profiles[7]}
-      />
-    </MemoryRouter>
-  ));
+  });
