317e2975d28d9c645cbae808213e1535051dec4a
Thilo Maier <git@maier.asia>
2018-08-13 17:10:02 -0400

    Fix import in ContentPage

--- src/templates/ContentPage/ContentPage.jsx
+++ src/templates/ContentPage/ContentPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-import Container from "../../components/Container";
+import Container from "../../helpers/Container";
 
 const Page = ({ children, description, pageTitle, siteTitle }) => (
   <Container maxWidth={7} mt={[3, 4]} mb={[3, 4]}>
