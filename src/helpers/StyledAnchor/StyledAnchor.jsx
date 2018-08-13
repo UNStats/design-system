d7d066b0a178d4ae929b4e087f447cc1aec016a3
Thilo Maier <git@maier.asia>
2018-08-13 10:28:13 -0400

    Replace global anchor style with StyledAnchor component

---
+++ src/helpers/StyledAnchor/StyledAnchor.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledAnchor = ({ Anchor, children, display, href }) => {
+  const Styled = styled(Anchor)`
+    height: 100%;
+    display: ${({ display: d }) => d};
+    color: inherit;
+    text-decoration: none;
+    transition: all 0.2s ease-out;
+    &:hover {
+      opacity: 0.5;
+      transition: all 0.2s ease-in;
+    }
+  `;
+  return (
+    <Styled display={display} href={href}>
+      {children}
+    </Styled>
+  );
+};
+
+StyledAnchor.propTypes = {
+  Anchor: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  display: PropTypes.oneOf(["block", "inline"]).isRequired,
+  href: PropTypes.string.isRequired
+};
+
+export default StyledAnchor;
