8619cdcb80f2e5cb0be8851a280f1479d3ed5171
Thilo Maier <git@maier.asia>
2018-08-15 12:54:28 -0400

    Move Storybook config to config folder

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -2,8 +2,8 @@ import React from "react";
 import { configure, addDecorator } from "@storybook/react";
 import { setOptions } from "@storybook/addon-options";
 import "@storybook/addon-console";
-import Provider from "../src/components/Provider";
-import pkg from "../package.json";
+import Provider from "../../src/components/Provider";
+import pkg from "../../package.json";
 
 // Set Storybook UI options.
 setOptions({
@@ -12,9 +12,11 @@ setOptions({
 
 // Add decorators before require.context:
 // https://github.com/storybooks/storybook/issues/3246
+
+// eslint-disable-next-line react/jsx-filename-extension
 addDecorator(story => <Provider>{story()}</Provider>);
 
-const req = require.context("../src", true, /\.stories\.jsx$/);
+const req = require.context("../../src", true, /\.stories\.jsx$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
