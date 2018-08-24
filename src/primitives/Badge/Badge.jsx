a91c23e8e3804059df797b1a000ddb943369f161
Thilo Maier <git@maier.asia>
2018-08-24 22:00:36 +0200

    Use primitives to render Badge

--- src/primitives/Badge/Badge.jsx
+++ src/primitives/Badge/Badge.jsx
@@ -1,47 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { fontFamily, fontSize, fontWeight, space } from "styled-system";
 import Anchor from "../Anchor/Anchor";
 import Link from "../Link";
+import Text from "../Text";
 import { colors } from "../../theme";
 
-const Wrapper = styled.div`
+const Pill = styled(Text.span)`
   display: inline-block;
-  ${space};
-`;
-
-const Pill = styled.div`
   border-radius: 9999px;
   color: ${({ theme, color }) => theme.colors[theme.colorSchemes[color].color]};
   background-color: ${({ theme, color }) =>
     theme.colors[theme.colorSchemes[color].bgColor]};
-  ${fontFamily};
-  ${fontSize};
-  ${fontWeight};
-  ${space};
 `;
 
 const Badge = ({ Anchor: A, href, color, children, ...props }) => (
-  <Wrapper {...props}>
-    <Link Anchor={A} display="block" href={href}>
-      <Pill
-        color={color}
-        fontFamily="sansSerif"
-        fontSize={7}
-        fontWeight="bold"
-        px={2}
-        py={1}
-      >
-        {children}
-      </Pill>
-    </Link>
-  </Wrapper>
+  <Link Anchor={A} display="inline-block" href={href} {...props}>
+    <Pill
+      color={color}
+      fontFamily="sansSerif"
+      fontSize={7}
+      fontWeight="bold"
+      px={2}
+      py={1}
+    >
+      {children}
+    </Pill>
+  </Link>
 );
 
 Badge.propTypes = {
   Anchor: PropTypes.func,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.string.isRequired,
   color: PropTypes.oneOf(Object.keys(colors)),
   href: PropTypes.string.isRequired
 };
--- src/primitives/Badge/Badge.stories.jsx
+++ src/primitives/Badge/Badge.stories.jsx
@@ -1,51 +1,51 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { Box } from "grid-styled";
 import Badge from "./Badge";
 import NewTabAnchor from "../NewTabAnchor";
 
 storiesOf("Primitives/Badge", module)
-  .addDecorator(story => <Box m={1}>{story()}</Box>)
   .addWithPercyOptions("default color", { skip: true }, () => (
-    <Badge href="#">default color</Badge>
+    <Badge href="#" m={2}>
+      default color
+    </Badge>
   ))
   .addWithPercyOptions("primary color", { skip: true }, () => (
-    <Badge color="primary" href="#">
+    <Badge color="primary" href="#" m={2}>
       primary color
     </Badge>
   ))
   .addWithPercyOptions("blue", { skip: true }, () => (
-    <Badge color="blue" href="#">
+    <Badge color="blue" href="#" m={2}>
       blue
     </Badge>
   ))
   .addWithPercyOptions("green", { skip: true }, () => (
-    <Badge color="green" href="#">
+    <Badge color="green" href="#" m={2}>
       green
     </Badge>
   ))
   .addWithPercyOptions("red", { skip: true }, () => (
-    <Badge color="red" href="#">
+    <Badge color="red" href="#" m={2}>
       red
     </Badge>
   ))
   .addWithPercyOptions("yellow", { skip: true }, () => (
-    <Badge color="yellow" href="#">
+    <Badge color="yellow" href="#" m={2}>
       yellow
     </Badge>
   ))
   .addWithPercyOptions("gray", { skip: true }, () => (
-    <Badge color="gray" href="#">
+    <Badge color="gray" href="#" m={2}>
       gray
     </Badge>
   ))
   .addWithPercyOptions("black", { skip: true }, () => (
-    <Badge color="black" href="#">
+    <Badge color="black" href="#" m={2}>
       gray
     </Badge>
   ))
   .addWithPercyOptions("custom anchor", { skip: true }, () => (
-    <Badge href="#" Anchor={NewTabAnchor}>
+    <Badge href="#" Anchor={NewTabAnchor} m={2}>
       custom anchor
     </Badge>
   ));
