316e6e93d8757da40d36ca7abb267989ca335bd0
Thilo Maier <git@maier.asia>
2018-08-24 22:23:06 +0200

    Rewrite SiteTitle with primitives

--- src/primitives/SiteTitle/SiteTitle.jsx
+++ src/primitives/SiteTitle/SiteTitle.jsx
@@ -1,53 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import {
-  color as colorUtility,
-  display,
-  flexDirection,
-  fontFamily,
-  fontSize,
-  space
-} from "styled-system";
+import { display } from "styled-system";
+import { Flex } from "grid-styled";
+import Text from "../Text";
 import { colors } from "../../theme";
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: bold;
-  ${colorUtility};
-  ${flexDirection};
-  ${fontFamily};
-`;
-
-const Organization = styled.div`
+const NoWrapText = styled(Text)`
   white-space: nowrap;
-  ${fontSize};
-  ${space};
 `;
 
 const Hide = styled.span`
   ${display};
 `;
 
-const Event = styled.div`
-  white-space: nowrap;
-  ${fontSize};
-`;
-
 const SiteTitle = ({ color }) => (
-  <Wrapper
-    color={color}
+  <Flex
     flexDirection={["row", "column"]}
-    fontFamily="sansSerif"
+    justifyContent="center"
+    alignItems="center"
   >
-    <Organization fontSize={[4, 5, 4]} mr={[2, 0]}>
+    <NoWrapText
+      color={color}
+      fontFamily="sansSerif"
+      fontSize={[4, 5, 4]}
+      fontWeight="bold"
+      mr={[2, 0]}
+    >
       U<Hide display={["none", "inline"]}>nited </Hide>N
       <Hide display={["none", "inline"]}>ations</Hide>
-    </Organization>
-    <Event fontSize={[4, 4, 3]}>World Data Forum</Event>
-  </Wrapper>
+    </NoWrapText>
+    <NoWrapText
+      color={color}
+      fontFamily="sansSerif"
+      fontSize={[4, 4, 3]}
+      fontWeight="bold"
+    >
+      World Data Forum
+    </NoWrapText>
+  </Flex>
 );
 
 SiteTitle.propTypes = {
