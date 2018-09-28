ac12705bcbf76d6b3bad68e8580ad5b4c71cc2c5
Thilo Maier <git@maier.asia>
2018-09-28 15:28:34 -0400

    Upgrade Provider to styled-components v4

--- src/helpers/Provider/Provider.jsx
+++ src/helpers/Provider/Provider.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import { ThemeProvider, injectGlobal } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { normalize } from "polished";
 import theme from "../../theme";
 
 // https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/
-injectGlobal`
+const GlobalStyle = createGlobalStyle`
   ${normalize()};
   html {
     box-sizing: border-box;
@@ -17,11 +17,12 @@ injectGlobal`
   }
 `;
 
-// ThemeProvider can have one child only.
-// With div added, Provider can have multiple children.
 const Provider = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <div>{children}</div>
+    <Fragment>
+      <GlobalStyle />
+      {children}
+    </Fragment>
   </ThemeProvider>
 );
 
