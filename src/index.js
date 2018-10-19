362c93410a42188e30319619a28914e55fe8adfc
Thilo Maier <git@maier.asia>
2018-10-18 21:29:24 -0400

    Remove Anchor and Link components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import AlternateSiteTitle from "./primitives/AlternateSiteTitle";
-import Anchor from "./primitives/Anchor";
 import Head from "./helpers/Head";
 import Header from "./composites/Header";
 import HeadingPage from "./pages/HeadingPage";
-import Link from "./primitives/Link";
-import NewTabAnchor from "./primitives/NewTabAnchor";
+import NewTabAnchor from "./primitives/NewTabLink";
 import Page from "./pages/Page";
 import PreviewList from "./composites/PreviewList";
 import ProfilePreview from "./composites/ProfilePreview";
@@ -13,11 +11,9 @@ import SiteTitle from "./primitives/SiteTitle";
 
 export {
   AlternateSiteTitle,
-  Anchor,
   Head,
   Header,
   HeadingPage,
-  Link,
   NewTabAnchor,
   Page,
   PreviewList,
