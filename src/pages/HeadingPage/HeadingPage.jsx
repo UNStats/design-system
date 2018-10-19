fe00f004128eb77aa7743d14ab218912721e97ba
Thilo Maier <git@maier.asia>
2018-10-18 21:47:06 -0400

    Remove Text and Heading

--- src/pages/HeadingPage/HeadingPage.jsx
+++ src/pages/HeadingPage/HeadingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Heading from "../../primitives/Heading";
+import { Text } from "rebass";
 import Page from "../Page";
 
 const HeadingPage = ({ title, description, children }) => (
   <Page title={title} description={description}>
-    <Heading.h1 mb={4}>{title}</Heading.h1>
+    <Text as="h1" mb={4}>
+      {title}
+    </Text>
     {children}
   </Page>
 );
