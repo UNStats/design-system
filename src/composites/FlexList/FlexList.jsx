46bb63d62a6ec341773bde9553670b34378d1280
Thilo Maier <git@maier.asia>
2018-10-31 13:56:29 -0400

    Remove gap prop from FlexList

--- src/composites/FlexList/FlexList.jsx
+++ src/composites/FlexList/FlexList.jsx
@@ -2,24 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Flex } from "rebass";
 
-const FlexList = ({ color, gap, link, render, values, ...props }) => (
+const FlexList = ({ color, link, render, values, ...props }) => (
   <Flex {...props} flexWrap="wrap" justifyContent="center">
     {values.map(value => render(value))}
   </Flex>
 );
 
 FlexList.propTypes = {
-  gap: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-    PropTypes.array
-  ]),
   render: PropTypes.func.isRequired,
   values: PropTypes.arrayOf(PropTypes.object).isRequired
-};
-
-FlexList.defaultProps = {
-  gap: 3
 };
 
 export default FlexList;
