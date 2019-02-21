7509207329304ce1da41e1b7bc5a03d2318feb3a
Thilo Maier <git@maier.asia>
2019-02-21 21:02:56 +0000

    Use gatsby-theme-undataforum for docs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,9 @@
 const pkg = require("./package.json");
 
 module.exports = {
+  __experimentalThemes: ["gatsby-theme-undataforum"],
   siteMetadata: {
     title: pkg.name,
-    siteUrl: pkg.homepage,
-    sidebar: {
-      pages: [
-        {
-          slug: "/about",
-          title: "about"
-        }
-      ]
-    }
-  },
-  pathPrefix: "/",
-  plugins: [
-    {
-      resolve: `gatsby-mdx`,
-
-      options: {
-        extensions: [".mdx", ".md"],
-        defaultLayouts: {
-          default: require.resolve("./src/templates/ComponentPage.jsx")
-        }
-      }
-    }
-  ]
+    description: pkg.description
+  }
 };
