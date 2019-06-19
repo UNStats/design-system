b1ceca4895f4a988d8ecc2b4985f1ec88e38fc24
Thilo Maier <git@maier.asia>
2019-06-19 16:11:03 -0400

    feat: Add Twitter icon (closes #17)

---
+++ packages/components/src/icons/TwitterIcon/TwitterIcon.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Text } from 'rebass';
+import TwitterIcon from './TwitterIcon';
+
+storiesOf('Icons/TwitterIcon', module)
+  .add('display inline', () => (
+    <Text m={2}>
+      This icon <TwitterIcon width={32} /> is rendered inline.
+    </Text>
+  ))
+  .add('add color and spacing', () => (
+    <Text color="green" m={2}>
+      This icon <TwitterIcon width={32} mx={2} /> is rendered inline with color
+      and spacing.
+    </Text>
+  ));
