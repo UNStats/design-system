acd85f48cc4600d005c04c2c5330707154828192
Thilo Maier <git@maier.asia>
2019-06-20 16:36:23 -0400

    feat: Add Footer (closes #3)

---
+++ packages/components/src/components/Footer/Footer.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { arrayOf, func, string } from 'prop-types';
+import { Flex, Heading } from 'rebass';
+import { withTheme } from 'styled-components';
+import { themeGet } from '@styled-system/theme-get';
+import { linkType, modeType } from '../../types';
+import Container from '../Container';
+import FlexList from '../FlexList';
+import SmartLink from '../SmartLink';
+
+const Footer = ({ logo, title, social, links, mode, ...props }) => {
+  const color = themeGet(`colors.modes.${mode}.text`)(props);
+  const bg = themeGet(`colors.modes.${mode}.background`)(props);
+  return (
+    <Container {...props} maxWidth={9}>
+      <Flex
+        as="footer"
+        flexDirection="column"
+        alignItems="center"
+        color={color}
+        bg={bg}
+        p={[2, 3]}
+      >
+        {logo && (
+          <SmartLink css="height: 100%;" href="/" color={color} mb={[1, 2]}>
+            {logo()}
+          </SmartLink>
+        )}
+        {title && (
+          <SmartLink href="/">
+            <Heading
+              css="white-space: nowrap;"
+              color={color}
+              fontSize={[3, 4, 5]}
+              mb={[2, 3]}
+            >
+              {title}
+            </Heading>
+          </SmartLink>
+        )}
+        {social && social()}
+        <FlexList
+          render={link => (
+            <SmartLink
+              color={color}
+              href={link.href}
+              key={link}
+              py={[1, 2]}
+              px={[2, 3]}
+            >
+              {link.text}
+            </SmartLink>
+          )}
+          values={links}
+        />
+      </Flex>
+    </Container>
+  );
+};
+
+Footer.propTypes = {
+  logo: func,
+  title: string,
+  social: func,
+  links: arrayOf(linkType).isRequired,
+  mode: modeType,
+};
+
+Footer.defaultProps = {
+  mode: 'primary',
+};
+
+export default withTheme(Footer);
---
+++ packages/components/src/components/Footer/Footer.stories.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, select } from '@storybook/addon-knobs';
+import { DummyLogo } from '../../helpers';
+import FlexList from '../FlexList';
+import { GitHubIcon, TwitterIcon } from '../../icons';
+import SmartLink from '../SmartLink';
+import Footer from './Footer';
+
+const modes = {
+  primary: 'primary',
+  secondary: 'secondary',
+  blue: 'blue',
+  green: 'green',
+  red: 'red',
+  yellow: 'yellow',
+};
+
+const renderGitHubIcon = () => (
+  <SmartLink href="https://github.com/UNDataForum" color="inherit">
+    <GitHubIcon width={[32, 48]} p={[1, 2]} />
+  </SmartLink>
+);
+
+const renderTwitterIcon = () => (
+  <SmartLink href="https://twitter.com/UNDataForum" color="inherit">
+    <TwitterIcon width={[32, 48]} p={[1, 2]} />
+  </SmartLink>
+);
+
+storiesOf('Components/Footer', module)
+  .addDecorator(withKnobs)
+  .add('everything', () => (
+    <Footer
+      logo={() => <DummyLogo height={3} />}
+      title="Footer Title"
+      social={() => (
+        <FlexList
+          render={renderIcon => renderIcon()}
+          values={[renderTwitterIcon, renderGitHubIcon]}
+          mb={[1, 2]}
+        />
+      )}
+      links={[
+        { text: 'Contact', href: '/contact' },
+        { text: 'Copyright', href: '/copyright' },
+        { text: 'Fraud Alert', href: '/fraud-alert' },
+        { text: 'Privacy Notice', href: '/privacy-notice' },
+        { text: 'Terms of Use', href: '/terms-of-use' },
+      ]}
+      mode={select('mode', modes, 'primary')}
+    />
+  ))
+  .add('logo only', () => (
+    <Footer
+      logo={() => <DummyLogo height={3} />}
+      links={[
+        { text: 'Contact', href: '/contact' },
+        { text: 'Copyright', href: '/copyright' },
+        { text: 'Fraud Alert', href: '/fraud-alert' },
+        { text: 'Privacy Notice', href: '/privacy-notice' },
+        { text: 'Terms of Use', href: '/terms-of-use' },
+      ]}
+      mode={select('mode', modes, 'primary')}
+    />
+  ))
+  .add('title only', () => (
+    <Footer
+      title="Footer Title"
+      links={[
+        { text: 'Contact', href: '/contact' },
+        { text: 'Copyright', href: '/copyright' },
+        { text: 'Fraud Alert', href: '/fraud-alert' },
+        { text: 'Privacy Notice', href: '/privacy-notice' },
+        { text: 'Terms of Use', href: '/terms-of-use' },
+      ]}
+      mode={select('mode', modes, 'primary')}
+    />
+  ))
+  .add('social only', () => (
+    <Footer
+      social={() => (
+        <FlexList
+          render={renderIcon => renderIcon()}
+          values={[renderTwitterIcon, renderGitHubIcon]}
+          mb={[1, 2]}
+        />
+      )}
+      links={[
+        { text: 'Contact', href: '/contact' },
+        { text: 'Copyright', href: '/copyright' },
+        { text: 'Fraud Alert', href: '/fraud-alert' },
+        { text: 'Privacy Notice', href: '/privacy-notice' },
+        { text: 'Terms of Use', href: '/terms-of-use' },
+      ]}
+      mode={select('mode', modes, 'primary')}
+    />
+  ))
+  .add('minimalistic', () => (
+    <Footer
+      links={[
+        { text: 'Contact', href: '/contact' },
+        { text: 'Copyright', href: '/copyright' },
+        { text: 'Fraud Alert', href: '/fraud-alert' },
+        { text: 'Privacy Notice', href: '/privacy-notice' },
+        { text: 'Terms of Use', href: '/terms-of-use' },
+      ]}
+      mode={select('mode', modes, 'primary')}
+    />
+  ))
+  .add('custom mode', () => (
+    <Footer
+      logo={() => <DummyLogo height={3} />}
+      title="Footer Title"
+      social={() => (
+        <FlexList
+          render={renderIcon => renderIcon()}
+          values={[renderTwitterIcon, renderGitHubIcon]}
+          mb={[1, 2]}
+        />
+      )}
+      links={[
+        { text: 'Contact', href: '/contact' },
+        { text: 'Copyright', href: '/copyright' },
+        { text: 'Fraud Alert', href: '/fraud-alert' },
+        { text: 'Privacy Notice', href: '/privacy-notice' },
+        { text: 'Terms of Use', href: '/terms-of-use' },
+      ]}
+      mode={select('mode', modes, 'yellow')}
+    />
+  ));
