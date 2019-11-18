4a5728b705244afa0cc09c93e3866abe7c120010
Thilo Maier <git@maier.asia>
2019-11-18 10:29:05 -0500

    Always stretch Footer to edges GH-81

--- packages/components/src/components/Footer/Footer.js
+++ packages/components/src/components/Footer/Footer.js
@@ -1,55 +1,59 @@
 import React from 'react';
 import { arrayOf, func, string, oneOf } from 'prop-types';
-import { Flex, Heading, Text } from '@theme-ui/components';
+import { Box, Flex, Heading, Text } from '@theme-ui/components';
 
 import Container from '../Container';
 import SmartLink, { linkType } from '../SmartLink';
 
 const Footer = ({ logo, title, social, links, variant = 'primary' }) => {
   return (
-    <Container p={[2, 3, 4]} maxWidth="wide" variant={`pairings.${variant}`}>
-      <Flex
-        as="footer"
-        sx={{
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        {logo && (
-          <SmartLink href="/" variant={variant} height="100%" mb={[1, 2]}>
-            {logo()}
-          </SmartLink>
-        )}
-        {title && (
-          <SmartLink href="/" variant={variant}>
-            <Heading
-              sx={{
-                whiteSpace: 'nowrap',
-                fontFamily: 'heading',
-                fontSize: [3, 4, 5],
-                mb: [2, 3],
-              }}
-            >
-              {title}
-            </Heading>
-          </SmartLink>
-        )}
-        {social && social(variant)}
-        <Flex sx={{ flexWrap: 'wrap', justifyContent: 'center' }}>
-          {links.map(link => (
-            <SmartLink
-              href={link.href}
-              key={link.href}
-              px={[2, 3]}
-              py={[1, 2]}
-              variant={variant}
-            >
-              <Text sx={{ fontFamily: 'body' }}>{link.text}</Text>
+    <Box sx={{ variant: `pairings.${variant}` }}>
+      <Container p={[2, 3, 4]} maxWidth="wide">
+        <Flex
+          as="footer"
+          sx={{
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {logo && (
+            <SmartLink href="/" variant={variant} height="100%" mb={[1, 2]}>
+              {logo()}
             </SmartLink>
-          ))}
+          )}
+          {title && (
+            <SmartLink href="/" variant={variant}>
+              <Heading
+                sx={{
+                  whiteSpace: 'nowrap',
+                  fontFamily: 'heading',
+                  fontSize: [3, 4, 5],
+                  mb: [2, 3],
+                }}
+              >
+                {title}
+              </Heading>
+            </SmartLink>
+          )}
+          {social && social(variant)}
+          <Flex sx={{ flexWrap: 'wrap', justifyContent: 'center' }}>
+            {links.map(link => (
+              <SmartLink
+                href={link.href}
+                key={link.href}
+                px={[2, 3]}
+                py={[1, 2]}
+                variant={variant}
+              >
+                <Text sx={{ fontFamily: 'body', textAlign: 'center' }}>
+                  {link.text}
+                </Text>
+              </SmartLink>
+            ))}
+          </Flex>
         </Flex>
-      </Flex>
-    </Container>
+      </Container>
+    </Box>
   );
 };
 
