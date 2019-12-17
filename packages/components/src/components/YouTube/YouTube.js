d9e64c8e65beedecb6770e3acac524384ed6fbfb
Thilo Maier <git@maier.asia>
2019-12-17 10:30:10 -0500

    Undo support for changeset props GH-269

--- packages/components/src/components/YouTube/YouTube.js
+++ packages/components/src/components/YouTube/YouTube.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { number, string } from 'prop-types';
 import { AspectRatio } from '@theme-ui/components';
 
+// Don't add ...props because of https://github.com/UNDataForum/design-system/issues/269.
+// To add spacing around videos use Container or Box.
 // Player parameters: https://developers.google.com/youtube/player_parameters.
-const YouTube = ({ id, title, ratio = 16 / 9, ...props }) => (
-  <AspectRatio {...props} ratio={ratio}>
+const YouTube = ({ id, title, ratio = 16 / 9 }) => (
+  <AspectRatio ratio={ratio}>
     <iframe
       width="100%"
       height="100%"
