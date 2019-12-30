ad237ff6162f9ff00fbd9c1faa4a5305fb3eb3b9
Thilo Maier <git@maier.asia>
2019-12-30 14:40:44 -0500

    Add custom colors support to PostPreview GH-289

--- packages/components/src/components/PostPreview/PostPreview.js
+++ packages/components/src/components/PostPreview/PostPreview.js
@@ -5,17 +5,28 @@ import { Button, Flex, Heading, Text } from '@theme-ui/components';
 import { responsiveNumberType } from '../../types';
 import SmartLink from '../SmartLink';
 
-const PostPreview = ({ post, fontSize = [3, 4], ...props }) => {
+const PostPreview = ({
+  post,
+  fontSize = [3, 4],
+  colors = { text: 'text', background: 'background', accent: 'primary' },
+  ...props
+}) => {
   const mb = post.description ? 3 : 0;
   // Use flexDirection prop instead of sx prop to ensure it cannot be overridden.
   // Set m={0} in Heading due to https://github.com/styled-system/styled-system/issues/644.
   return (
-    <Flex {...props} sx={{ flexDirection: 'column' }}>
+    <Flex
+      {...props}
+      sx={{
+        flexDirection: 'column',
+        fontFamily: 'body',
+        color: colors.text,
+        bg: colors.background,
+      }}
+    >
       <Heading
         as="h1"
         sx={{
-          color: 'text',
-          fontFamily: 'body',
           fontSize,
           lineHeight: 'heading',
           m: 0,
@@ -26,18 +37,13 @@ const PostPreview = ({ post, fontSize = [3, 4], ...props }) => {
         {post.title}
       </Heading>
       {post.authors()}
-      <Text
-        as="time"
-        sx={{ color: 'text', display: 'block', fontFamily: 'body', mb }}
-      >
+      <Text as="time" sx={{ display: 'block', mb }}>
         {post.date}
       </Text>
       {post.description && (
         <Text
           as="p"
           sx={{
-            color: 'text',
-            fontFamily: 'body',
             lineHeight: 'body',
             my: 0,
           }}
@@ -57,8 +63,9 @@ const PostPreview = ({ post, fontSize = [3, 4], ...props }) => {
             as={SmartLink}
             sx={{
               alignSelf: ['stretch', 'flex-start'],
-              fontFamily: 'body',
               whiteSpace: 'nowrap',
+              color: colors.background,
+              bg: colors.accent,
               mt: 3,
             }}
             href={post.href}
@@ -83,6 +90,11 @@ export const postType = shape({
 PostPreview.propTypes = {
   post: postType.isRequired,
   fontSize: responsiveNumberType,
+  colors: shape({
+    text: string.isRequired,
+    background: string.isRequired,
+    accent: string.isRequired,
+  }),
 };
 
 export default PostPreview;
