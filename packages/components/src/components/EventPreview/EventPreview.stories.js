b582b7b11609c8fbf6e2409617e1c79e8442899b
Thilo Maier <git@maier.asia>
2019-12-30 14:03:11 -0500

    Add custom colors support to EventPreview GH-287

--- packages/components/src/components/EventPreview/EventPreview.stories.js
+++ packages/components/src/components/EventPreview/EventPreview.stories.js
@@ -98,7 +98,7 @@ storiesOf('Components/EventPreview', module)
         },
         description() {
           return (
-            <Text as="p" sx={{ fontFamily: 'body', lineHeight: 'body', mb: 3 }}>
+            <Text as="p" sx={{ lineHeight: 'body', mb: 3 }}>
               Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed
               interdum venenatis, turpis enim blandit mi, in porttitor pede
               justo eu massa. Donec dapibus. Duis at velit eu est congue
@@ -144,7 +144,7 @@ storiesOf('Components/EventPreview', module)
         duration: 'display duration',
         description() {
           return (
-            <Text as="p" sx={{ fontFamily: 'body', lineHeight: 'body', mb: 3 }}>
+            <Text as="p" sx={{ lineHeight: 'body', mb: 3 }}>
               Totam rem aperiam. Inventore veritatis et quasi architecto beatae
               vitae dicta sunt explicabo. Lorem ipsum dolor sit amet,
               consectetur adipisicing elit.
@@ -175,7 +175,7 @@ storiesOf('Components/EventPreview', module)
         duration: 'display duration',
         description() {
           return (
-            <Text as="p" sx={{ fontFamily: 'body', lineHeight: 'body', mb: 3 }}>
+            <Text as="p" sx={{ lineHeight: 'body', mb: 3 }}>
               Laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim
               veniam, quis nostrud exercitation ullamco. Lorem ipsum dolor sit
               amet, consectetur adipisicing elit. Do eiusmod tempor incididunt
@@ -208,7 +208,7 @@ storiesOf('Components/EventPreview', module)
         duration: 'display duration',
         description() {
           return (
-            <Text as="p" sx={{ fontFamily: 'body', lineHeight: 'body', mb: 3 }}>
+            <Text as="p" sx={{ lineHeight: 'body', mb: 3 }}>
               Corrupti quos dolores et quas molestias excepturi sint occaecati.
               Animi, id est laborum et dolorum fuga. Temporibus autem quibusdam
               et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
@@ -248,7 +248,7 @@ storiesOf('Components/EventPreview', module)
         duration: 'display duration',
         description() {
           return (
-            <Text as="p" sx={{ fontFamily: 'body', lineHeight: 'body', mb: 3 }}>
+            <Text as="p" sx={{ lineHeight: 'body', mb: 3 }}>
               Nisi ut aliquid ex ea commodi consequatur? Facere possimus, omnis
               voluptas assumenda est, omnis dolor repellendus. Quis autem vel
               eum iure reprehenderit qui in ea voluptate velit esse quam.
@@ -270,9 +270,110 @@ storiesOf('Components/EventPreview', module)
       >
         <Grid gap={4} columns={[1, null, 2]}>
           {events.map(({ id, ...event }) => (
-            <EventPreview event={{ ...event }} key={id} />
+            <EventPreview
+              event={{ ...event }}
+              colors={{
+                text: 'text',
+                background: 'background',
+                accent: 'secondary',
+              }}
+              key={id}
+            />
           ))}
         </Grid>
       </Container>
     );
-  });
+  })
+  .add('custom colors', () => (
+    <EventPreview
+      event={{
+        type: 'Webinar',
+        title() {
+          return (
+            <Heading as="h1" sx={{ mb: 3 }}>
+              Vestibulum proin eu mi nulla ac enim in tempor turpis
+            </Heading>
+          );
+        },
+        date: 'October 14, 1983, 1:30 PM EDT',
+        duration: '90 minutes',
+        speakers() {
+          return (
+            <Avatars
+              values={[
+                {
+                  id: 'a6b4babd-6b11-4b81-8ac0-73cefb040cb7',
+                  avatar() {
+                    return (
+                      <Image
+                        alt="Portrait of Willow Rangall"
+                        sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                        src="/avatar0.png"
+                      />
+                    );
+                  },
+                  name: 'Willow Rangall (Moderator)',
+                  href: '/profiles/willow-rangall',
+                },
+                {
+                  id: '561285d3-4eed-473c-baba-6b3564f4e6a7',
+                  avatar() {
+                    return (
+                      <Image
+                        alt="Portrait of Suzy Cahan"
+                        sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                        src="/avatar1.png"
+                      />
+                    );
+                  },
+                  name: 'Suzy Cahan',
+                  href: '/profiles/suzy-cahan',
+                },
+                {
+                  id: '453d9d8a-b1e6-4cac-9fdd-b6c354fa3262',
+                  avatar() {
+                    return (
+                      <Image
+                        alt="Portrait of Sandra Abramovici"
+                        sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                        src="/avatar2.png"
+                      />
+                    );
+                  },
+                  name: 'Sandra Abramovici',
+                  href: '/profiles/sandra-abramovici',
+                },
+              ]}
+              colors={{
+                text: 'background',
+                background: 'secondary',
+                accent: 'background',
+              }}
+              mb={3}
+            />
+          );
+        },
+        description() {
+          return (
+            <Text as="p" sx={{ lineHeight: 'body', mb: 3 }}>
+              Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed
+              interdum venenatis, turpis enim blandit mi, in porttitor pede
+              justo eu massa. Donec dapibus. Duis at velit eu est congue
+              elementum. In hac habitasse platea dictumst. Morbi vestibulum,
+              velit id pretium iaculis, diam erat fermentum justo, nec
+              condimentum neque sapien placerat ante. Nulla justo.
+            </Text>
+          );
+        },
+        links: {
+          registration: '/register',
+        },
+      }}
+      colors={{
+        text: 'background',
+        background: 'secondary',
+        accent: 'background',
+      }}
+      p={2}
+    />
+  ));
