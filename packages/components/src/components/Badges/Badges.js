a5327e728b45c074d5125521226d75565623a1d4
Thilo Maier <git@maier.asia>
2019-12-30 11:08:46 -0500

    Refactor Badges GH-284

--- packages/components/src/components/Badges/Badges.js
+++ packages/components/src/components/Badges/Badges.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import { arrayOf } from 'prop-types';
+import { arrayOf, oneOf, string } from 'prop-types';
 import { Box, Flex } from '@theme-ui/components';
 
-import Badge, { badgeType } from '../Badge';
-import { alignType } from '../../types';
+import Badge from '../Badge';
 
 // Add margin around each badge.
 // Neutralize outer margins with negative margins on Flex.
-const Badges = ({ values, variant = 'center', ...props }) => (
+const Badges = ({
+  values,
+  color,
+  bg,
+  align = 'flex-start',
+  variant,
+  ...props
+}) => (
   <Box {...props}>
-    <Flex sx={{ flexWrap: 'wrap' }} variant={`badges.${variant}`} m={-1}>
+    <Flex sx={{ flexWrap: 'wrap', justifyContent: align }} m={-1}>
       {values.map(value => (
-        <Badge value={value} key={value.text} m={1} />
+        <Badge key={value} color={color} bg={bg} variant={variant} m={1}>
+          {value}
+        </Badge>
       ))}
     </Flex>
   </Box>
 );
 
+export const alignments = ['flex-start', 'center', 'flex-end'];
+export const alignType = oneOf(alignments);
+
 Badges.propTypes = {
-  values: arrayOf(badgeType).isRequired,
-  variant: alignType,
+  values: arrayOf(string).isRequired,
+  color: string.isRequired,
+  bg: string.isRequired,
+  align: alignType,
+  variant: oneOf(['outline']),
 };
 
 export default Badges;
--- packages/components/src/components/Badges/Badges.stories.js
+++ packages/components/src/components/Badges/Badges.stories.js
@@ -1,48 +1,64 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs';
+import { Box } from '@theme-ui/components';
 
-import { alignments } from '../../types';
-
-import Badges from './Badges';
+import Badges, { alignments } from './Badges';
 
 storiesOf('Components/Badges', module)
   .addDecorator(withKnobs)
   .add('a few badges', () => {
     return (
-      <Badges
-        values={[
-          { text: 'Design Engineer' },
-          { text: 'Recruiter', variant: 'secondary' },
-          { text: 'Teacher', variant: 'accent' },
-          { text: 'Scientist', variant: 'inverse' },
-        ]}
-        variant={select('variant', alignments, 'center')}
-        m={2}
-      />
+      <Box sx={{ fontFamily: 'body' }}>
+        <Badges
+          values={['Design Engineer', 'Recruiter', 'Teacher', 'Scientist']}
+          color="background"
+          bg={select('bg', ['primary', 'secondary', 'accent'], 'primary')}
+          align={select('align', alignments, 'flex-start')}
+          m={2}
+        />
+      </Box>
+    );
+  })
+  .add('outline', () => {
+    return (
+      <Box sx={{ fontFamily: 'body' }}>
+        <Badges
+          values={['Design Engineer', 'Recruiter', 'Teacher', 'Scientist']}
+          color={select('color', ['primary', 'secondary', 'accent'], 'primary')}
+          bg="background"
+          align={select('align', alignments, 'flex-start')}
+          variant="outline"
+          m={2}
+        />
+      </Box>
     );
   })
   .add('many badges', () => (
-    <Badges
-      values={[
-        { text: 'Design Engineer' },
-        { text: 'Recruiter', variant: 'secondary' },
-        { text: 'Teacher', variant: 'accent' },
-        { text: 'Environmental Specialist', variant: 'inverse' },
-        { text: 'Nuclear Power Engineer' },
-        { text: 'Cost Accountant', variant: 'secondary' },
-        { text: 'VP Marketing', variant: 'accent' },
-        { text: 'Automation Specialist', variant: 'inverse' },
-        { text: 'Analog Circuit Design Manager' },
-        { text: 'Senior Cost Accountant', variant: 'secondary' },
-        { text: 'Desktop Support Technician', variant: 'accent' },
-        { text: 'Web Developer', variant: 'inverse' },
-        { text: 'Technical Writer' },
-        { text: 'Database Administrator', variant: 'secondary' },
-        { text: 'Systems Administrator', variant: 'accent' },
-        { text: 'Assistant Manager', variant: 'inverse' },
-      ]}
-      variant={select('variant', alignments, 'center')}
-      m={2}
-    />
+    <Box sx={{ fontFamily: 'body' }}>
+      <Badges
+        values={[
+          'Design Engineer',
+          'Recruiter',
+          'Teacher',
+          'Environmental Specialist',
+          'Nuclear Power Engineer',
+          'Cost Accountant',
+          'VP Marketing',
+          'Automation Specialist',
+          'Analog Circuit Design Manager',
+          'Senior Cost Accountant',
+          'Desktop Support Technician',
+          'Web Developer',
+          'Technical Writer',
+          'Database Administrator',
+          'Systems Administrator',
+          'Assistant Manager',
+        ]}
+        color="background"
+        bg={select('bg', ['primary', 'secondary', 'accent'], 'primary')}
+        align={select('align', alignments, 'flex-start')}
+        m={2}
+      />
+    </Box>
   ));
