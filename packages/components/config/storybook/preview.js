63308f32b8cb79f584117b200b37ecc356dbff90
Thilo Maier <git@maier.asia>
2020-01-30 14:12:49 -0500

    Clean up Storybook config GH-363

--- packages/components/config/storybook/preview.js
+++ packages/components/config/storybook/preview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addDecorator, addParameters } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'theme-ui';
 import { withA11y } from '@storybook/addon-a11y';
 import preset from '@undataforum/preset';
@@ -11,23 +11,6 @@ import { NewTabLink } from '../../src/components';
 const theme = merge(preset, {
   link: {
     external: NewTabLink,
-  },
-  styles: {
-    root: {
-      body: { margin: 0 },
-    },
-  },
-});
-
-addParameters({
-  options: {
-    panelPosition: 'bottom',
-    // In v5.3.3 the sort order for stories is broken. This custom sort function fixes it.
-    // https://storybook.js.org/docs/configurations/options-parameter/#sorting-stories
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind
-        ? 0
-        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 });
 
