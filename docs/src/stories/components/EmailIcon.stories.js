50529eee7c74850d2a9513184db18ae2a1977798
Thilo Maier <git@maier.asia>
2020-02-27 11:18:35 -0500

    Refactor icons with generic Icon component GH-397

--- docs/src/stories/components/EmailIcon.stories.js
+++ docs/src/stories/components/EmailIcon.stories.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Box } from 'theme-ui';
 import { EmailIcon } from '@undataforum/components';
 
 storiesOf('Components/EmailIcon', module)
-  .add('scale to height', () => (
-    <Box sx={{ height: 64, m: 2 }}>
-      <EmailIcon scaleTo="height" />
-    </Box>
-  ))
-  .add('scale to width', () => (
-    <Box sx={{ width: 64, m: 2 }}>
-      <EmailIcon scaleTo="width" />
-    </Box>
-  ))
-  .add('custom color', () => (
-    <Box sx={{ height: 64, m: 2, color: 'primary' }}>
-      <EmailIcon scaleTo="height" />
-    </Box>
-  ));
+  .add('default', () => <EmailIcon />)
+  .add('custom size', () => <EmailIcon size={64} />)
+  .add('with title', () => <EmailIcon title="Send us an email" />)
+  .add('responsive size', () => <EmailIcon size={[32, 64, 128]} />);
---
+++ packages/components/src/icon.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { arrayOf, number, oneOfType } from 'prop-types';
+import { Box } from 'theme-ui';
+
+const Icon = ({ size = 32, ...props }) => (
+  <Box
+    {...props}
+    sx={{
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 1,
+      width: size,
+      height: size,
+      color: 'inherit',
+      bg: 'transparent',
+    }}
+  />
+);
+
+Icon.propTypes = {
+  size: oneOfType([number, arrayOf(number)]),
+};
+
+export default Icon;
