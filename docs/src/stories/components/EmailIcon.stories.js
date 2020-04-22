d6380ff3a0509590f6b078793ae059f41a46ce4c
Thilo Maier <git@maier.asia>
2020-04-22 00:00:33 -0400

    Add align prop to SocialIcons GH-455

--- docs/src/stories/components/EmailIcon.stories.js
+++ docs/src/stories/components/EmailIcon.stories.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { EmailIcon } from '@undataforum/components';
+import { Box } from 'theme-ui';
 
 storiesOf('Components/EmailIcon', module)
-  .add('default', () => <EmailIcon />)
-  .add('custom size', () => <EmailIcon size={64} />)
-  .add('with title', () => <EmailIcon title="Send us an email" />)
-  .add('responsive size', () => <EmailIcon size={[32, 64, 128]} />);
+  .add('default', () => (
+    <Box sx={{ m: 2 }}>
+      <EmailIcon />
+    </Box>
+  ))
+  .add('custom size', () => (
+    <Box sx={{ m: 2 }}>
+      <EmailIcon size={64} />
+    </Box>
+  ))
+  .add('with title', () => (
+    <Box sx={{ m: 2 }}>
+      <EmailIcon title="Send us an email" />
+    </Box>
+  ))
+  .add('responsive size', () => (
+    <Box sx={{ m: 2 }}>
+      <EmailIcon size={[32, 64, 128]} />
+    </Box>
+  ));
--- docs/src/stories/components/GitHubIcon.stories.js
+++ docs/src/stories/components/GitHubIcon.stories.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { GitHubIcon } from '@undataforum/components';
+import { Box } from 'theme-ui';
 
 storiesOf('Components/GitHubIcon', module)
-  .add('default', () => <GitHubIcon />)
-  .add('custom size', () => <GitHubIcon size={64} />)
-  .add('with title', () => <GitHubIcon title="Follow us on GitHub" />)
-  .add('responsive size', () => <GitHubIcon size={[32, 64, 128]} />);
+  .add('default', () => (
+    <Box sx={{ m: 2 }}>
+      <GitHubIcon />
+    </Box>
+  ))
+  .add('custom size', () => (
+    <Box sx={{ m: 2 }}>
+      <GitHubIcon size={64} />
+    </Box>
+  ))
+  .add('with title', () => (
+    <Box sx={{ m: 2 }}>
+      <GitHubIcon title="Follow us on GitHub" />
+    </Box>
+  ))
+  .add('responsive size', () => (
+    <Box sx={{ m: 2 }}>
+      <GitHubIcon size={[32, 64, 128]} />
+    </Box>
+  ));
