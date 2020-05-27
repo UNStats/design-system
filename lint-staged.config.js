2e1cb1fcc63b60f5919de7ec6ff2855d43e80e5f
Thilo Maier <git@maier.asia>
2020-05-27 09:23:48 -0400

    Remove `git add` from lint-staged.config.js Fixes #496

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,jsx}': ['eslint --cache --ext .js,.jsx --fix', 'git add'],
-  '*.{md,mdx,css,yaml,yml}': ['prettier --write', 'git add'],
+  '*.js': ['eslint --fix', 'prettier --write', 'prettier --check'],
+  '*.{md,mdx,css,yaml,yml}': ['prettier --write', 'prettier --check'],
 };
