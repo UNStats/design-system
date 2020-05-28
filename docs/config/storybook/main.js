b1796840c1fafac567d9960cc3268842beb43bd2
Thilo Maier <git@maier.asia>
2020-05-28 11:34:54 -0400

    Migrate stories for icons to MDX (#510)
    * Migrate GitHubIcon stories to MDX
    
    * Migrate TwitterIcon stories to MDX
    
    * Migrate EmailIcon stories to MDX
    
    * Move Storybook config folder back to default location
    
    * Remove @storybook/addon-storysource
    
    #508

--- docs/config/storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../../src/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-knobs',
-    '@storybook/addon-storysource',
-    '@storybook/addon-a11y',
-  ],
-};
