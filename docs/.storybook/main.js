28bea1a83ba4cc6ae67e4f52900e944c11aaae47
Thilo Maier <git@maier.asia>
2020-05-28 11:09:45 -0400

    Remove @storybook/addon-storysource

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-knobs',
-    '@storybook/addon-storysource',
     '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    // Sequence of addons is respected only when adding /register to knobs.
+    '@storybook/addon-knobs/register',
   ],
 };
