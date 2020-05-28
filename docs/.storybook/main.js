49fa66272b6bd7904e4f1251ddf20706086c0031
Thilo Maier <git@maier.asia>
2020-05-28 11:09:11 -0400

    Migrate GitHubIcon story to MDX

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-knobs',
     '@storybook/addon-storysource',
     '@storybook/addon-a11y',
