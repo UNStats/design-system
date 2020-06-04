11da1e946a91a63350515a432484419a0483d8aa
Thilo Maier <git@maier.asia>
2020-06-03 23:37:40 -0400

    Migrate stories for PostPreview to Doctocat #507

--- docs/src/pages/event-preview/grid-of-events.js
+++ docs/src/pages/event-preview/grid-of-events.js
@@ -7,7 +7,7 @@ const events = [
     id: '0e0dde9c-1cb3-4bbf-a070-d3b838d98398',
     tag: 'Webinar',
     title: (
-      <Heading as="h1" sx={{ textAlign: 'start', mb: 3 }}>
+      <Heading as="h2" mb={3}>
         Ut aut reiciendis voluptatibus maiores alias consequatur
       </Heading>
     ),
@@ -25,7 +25,7 @@ const events = [
     date: 'Display date',
     duration: 'display duration',
     description: (
-      <Text as="p" sx={{ lineHeight: 'body', textAlign: 'start', mb: 3 }}>
+      <Text as="p" variant="text.default" mb={3}>
         Totam rem aperiam. Inventore veritatis et quasi architecto beatae vitae
         dicta sunt explicabo. Lorem ipsum dolor sit amet, consectetur
         adipisicing elit.
@@ -38,7 +38,7 @@ const events = [
     id: 'c539b355-4729-4d8f-88eb-ee955e1ae832',
     tag: 'Session',
     title: (
-      <Heading as="h1" sx={{ textAlign: 'start', mb: 3 }}>
+      <Heading as="h2" mb={3}>
         Do eiusmod tempor incididunt ut labore et dolore magna aliqua
       </Heading>
     ),
@@ -46,7 +46,7 @@ const events = [
     date: 'Display date',
     duration: 'display duration',
     description: (
-      <Text as="p" sx={{ lineHeight: 'body', textAlign: 'start', mb: 3 }}>
+      <Text as="p" variant="text.default" mb={3}>
         Laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco. Lorem ipsum dolor sit amet,
         consectetur adipisicing elit. Do eiusmod tempor incididunt ut labore et
@@ -62,7 +62,7 @@ const events = [
     id: '60b7bd4e-b6d1-4f49-8f0a-4aaff9e55015',
     tag: 'Workshop',
     title: (
-      <Heading as="h1" sx={{ textAlign: 'start', mt: 1, mb: 3 }}>
+      <Heading as="h2" mb={3}>
         Et harum quidem rerum facilis est et expedita distinctio
       </Heading>
     ),
@@ -70,7 +70,7 @@ const events = [
     date: 'Display date',
     duration: 'display duration',
     description: (
-      <Text as="p" sx={{ lineHeight: 'body', textAlign: 'start', mb: 3 }}>
+      <Text as="p" variant="text.default" mb={3}>
         Corrupti quos dolores et quas molestias excepturi sint occaecati. Animi,
         id est laborum et dolorum fuga. Temporibus autem quibusdam et aut
         officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
@@ -84,7 +84,7 @@ const events = [
     id: 'e87d27fd-53e5-438a-93c0-32ea28e91f1a',
     tag: 'Panel',
     title: (
-      <Heading as="h1" sx={{ textAlign: 'start', mt: 1, mb: 3 }}>
+      <Heading as="h2" mb={3}>
         Itaque earum rerum hic tenetur a sapiente delectus
       </Heading>
     ),
@@ -97,7 +97,7 @@ const events = [
     date: 'Display date',
     duration: 'display duration',
     description: (
-      <Text as="p" sx={{ lineHeight: 'body', textAlign: 'start', mb: 3 }}>
+      <Text as="p" variant="text.default" mb={3}>
         Nisi ut aliquid ex ea commodi consequatur? Facere possimus, omnis
         voluptas assumenda est, omnis dolor repellendus. Quis autem vel eum iure
         reprehenderit qui in ea voluptate velit esse quam.
---
+++ docs/src/pages/post-preview/render-with-avatars.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { Heading, Image } from 'theme-ui';
+import { Avatars, PostPreview } from '@undataforum/components';
+
+const RenderWithNames = () => (
+  <PostPreview
+    post={{
+      title: (
+        <Heading as="h1" mb={3}>
+          Vestibulum proin eu mi nulla ac enim in tempor turpis
+        </Heading>
+      ),
+      date: 'May 31, 2018',
+      authors: (
+        <Avatars
+          values={[
+            {
+              id: 'a6b4babd-6b11-4b81-8ac0-73cefb040cb7',
+              avatar: (
+                <Image
+                  alt="Portrait of Willow Rangall"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar0.png"
+                />
+              ),
+              name: 'Willow Rangall',
+              href: '/profiles/willow-rangall',
+            },
+            {
+              id: '561285d3-4eed-473c-baba-6b3564f4e6a7',
+              avatar: (
+                <Image
+                  alt="Portrait of Suzy Cahan"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar1.png"
+                />
+              ),
+              name: 'Suzy Cahan',
+              href: '/profiles/suzy-cahan',
+            },
+            {
+              id: '453d9d8a-b1e6-4cac-9fdd-b6c354fa3262',
+              avatar: (
+                <Image
+                  alt="Portrait of Sandra Abramovici"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar2.png"
+                />
+              ),
+              name: 'Sandra Abramovici',
+              href: '/profiles/sandra-abramovici',
+            },
+            {
+              id: 'dfea2476-e3de-4b63-863e-a1c69af90138',
+              avatar: (
+                <Image
+                  alt="Portrait of Jan McKea"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar3.png"
+                />
+              ),
+              name: 'Jan McKea',
+              href: '/profiles/jan-mckea',
+            },
+            {
+              id: '15469d6c-cb5d-4750-8a0c-27a3b1beb187',
+              avatar: (
+                <Image
+                  alt="Portrait of Ola Brunger"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar4.png"
+                />
+              ),
+              name: 'Ola Brunger',
+              href: '/profiles/ola-brunger',
+            },
+            {
+              id: 'f4ee9708-9045-40e7-a899-b46841af488b',
+              avatar: (
+                <Image
+                  alt="Portrait of Ivette Rowlinson"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar5.png"
+                />
+              ),
+              name: 'Ivette Rowlinson',
+              href: '/profiles/ivette-rowlinson',
+            },
+            {
+              id: '9946a2cf-0774-41da-830e-bffe1259360f',
+              avatar: (
+                <Image
+                  alt="Portrait of Madeleine Stredwick"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar6.png"
+                />
+              ),
+              name: 'Madeleine Stredwick',
+              href: '/profiles/madeleine-stredwick',
+            },
+            {
+              id: 'd2e5b557-bf07-4e77-94f3-16bc1cd26a38',
+              avatar: (
+                <Image
+                  alt="Portrait of Lin Billson"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar7.png"
+                />
+              ),
+              name: 'Lin Billson',
+              href: '/profiles/lin-billson',
+            },
+            {
+              id: '9008c43d-49ab-4907-bdaa-28cb59f0596b',
+              avatar: (
+                <Image
+                  alt="Portrait of Alfie Piborn"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar8.png"
+                />
+              ),
+              name: 'Alfie Piborn',
+              href: '/profiles/alfie-piborn',
+            },
+            {
+              id: 'bfba8e8e-e052-4386-a781-5bc42eeb98b9',
+              avatar: (
+                <Image
+                  alt="Portrait of Garvy Bartlomiej"
+                  sx={{ borderRadius: 'circle', height: 64, width: 64 }}
+                  src="/avatar9.png"
+                />
+              ),
+              name: 'Garvy Bartlomiej',
+              href: '/profiles/garvy-bartlomiej',
+            },
+          ]}
+          mb={3}
+        />
+      ),
+    }}
+    m={2}
+  />
+);
+
+export default RenderWithNames;
