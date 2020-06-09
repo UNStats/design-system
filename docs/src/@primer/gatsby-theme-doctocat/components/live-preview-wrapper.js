48aa23d6c76de9d4469ff41425a44969c98c2e44
Thilo Maier <git@maier.asia>
2020-06-09 18:30:43 -0400

    Add links for visual regression testing Fixes #531

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { node } from 'prop-types';
-import { Box, ThemeProvider } from 'theme-ui';
-import preset from '@undataforum/preset';
+import { Box } from 'theme-ui';
 
+// No need to wrap with ThemeProvider since we already use gatsby-plugin-theme-ui.
 const LivePreviewWrapper = ({ children }) => (
-  <ThemeProvider theme={preset}>
-    <Box sx={{ m: 2 }}>{children}</Box>
-  </ThemeProvider>
+  <Box sx={{ m: 2 }}>{children}</Box>
 );
 
 LivePreviewWrapper.propTypes = {
