98a4b15acd54c19a59b3dbe66a942b65ed9dbd2f
Thilo Maier <git@maier.asia>
2020-06-24 17:56:27 -0400

    Fix the docs for ProfilePreview Fixes #549

--- docs/src/pages/profile-preview/align-start.js
+++ docs/src/pages/profile-preview/align-start.js
@@ -1,38 +1,41 @@
 import React from 'react';
-import { Image } from 'theme-ui';
+import { Image, ThemeProvider } from 'theme-ui';
+import preset from '@theme-ui/preset-base';
 import { ProfilePreview } from '@undataforum/components';
 
 const AlignStart = () => (
-  <ProfilePreview
-    profile={{
-      avatar: (
-        <Image
-          alt="Nance MacFadin"
-          sx={{ borderRadius: 'circle', height: 128, width: 128 }}
-          src="/avatar0.png"
-        />
-      ),
-      honorific: 'His Excellency',
-      name: 'Nance MacFadin',
-      jobtitle: 'Help Desk Operator',
-      organization: 'Schuster-Senger',
-      socialIcons: [
-        {
-          id: 'twitter',
-          username: 'nance-mcfadin',
-          title: 'Follow me on Twitter',
-        },
-        {
-          id: 'github',
-          username: 'nance-mcfadin',
-          title: 'Follow me on GitHub',
-        },
-      ],
-      badges: ['Committee Member', 'Keynote Speaker'],
-    }}
-    align="start"
-    m={2}
-  />
+  <ThemeProvider theme={preset}>
+    <ProfilePreview
+      profile={{
+        avatar: (
+          <Image
+            alt="Nance MacFadin"
+            sx={{ borderRadius: '50%', height: 128, width: 128 }}
+            src="/avatar0.png"
+          />
+        ),
+        honorific: 'His Excellency',
+        name: 'Nance MacFadin',
+        jobtitle: 'Help Desk Operator',
+        organization: 'Schuster-Senger',
+        socialIcons: [
+          {
+            id: 'twitter',
+            username: 'nance-mcfadin',
+            title: 'Follow me on Twitter',
+          },
+          {
+            id: 'github',
+            username: 'nance-mcfadin',
+            title: 'Follow me on GitHub',
+          },
+        ],
+        badges: ['Committee Member', 'Keynote Speaker'],
+      }}
+      align="start"
+      m={2}
+    />
+  </ThemeProvider>
 );
 
 export default AlignStart;
