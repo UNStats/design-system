fb1436aa4698596c629ab69a258fb056e8d62ed7
Thilo Maier <git@maier.asia>
2020-06-24 17:56:27 -0400

    Fix the docs for ProfilePreview Fixes #549

--- docs/src/pages/profile-preview/really-long-strings.js
+++ docs/src/pages/profile-preview/really-long-strings.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import { Image } from 'theme-ui';
+import { Image, ThemeProvider } from 'theme-ui';
+import preset from '@theme-ui/preset-base';
 import { ProfilePreview } from '@undataforum/components';
 
 const ReallyLongStrings = () => (
-  <ProfilePreview
-    profile={{
-      avatar: (
-        <Image
-          alt="Really really really really really really really really really really long name"
-          sx={{ borderRadius: 'circle', height: 128, width: 128 }}
-          src="/avatar7.png"
-        />
-      ),
-      name:
-        'Really really really really really really really really really really long name',
-      jobtitle:
-        'Really really really really really really really really really really long jobtitle',
-      organization:
-        'Really really really really really really really really really really long organization',
-      badges: ['Committee Member', 'Keynote Speaker'],
-    }}
-    m={2}
-  />
+  <ThemeProvider theme={preset}>
+    <ProfilePreview
+      profile={{
+        avatar: (
+          <Image
+            alt="Really really really really really really really really really really long name"
+            sx={{ borderRadius: '50%', height: 128, width: 128 }}
+            src="/avatar7.png"
+          />
+        ),
+        name:
+          'Really really really really really really really really really really long name',
+        jobtitle:
+          'Really really really really really really really really really really long jobtitle',
+        organization:
+          'Really really really really really really really really really really long organization',
+        badges: [
+          'Nulla officia fugiat',
+          'Dolor voluptate proident',
+          'Laboris anim cillum et dolore',
+          'Magna excepteur',
+          'Fugiat officia',
+          'Ut veniam nostrud',
+          'Ad ut labore nulla dolor velit',
+          'Ex officia',
+          'Aliquip',
+          'Incididunt consectetur',
+        ],
+      }}
+      m={2}
+    />
+  </ThemeProvider>
 );
 
 export default ReallyLongStrings;
