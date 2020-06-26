e8585c4381489d138ae0ee3b000bf1becc4161d2
Thilo Maier <git@maier.asia>
2020-06-26 10:23:57 -0400

    Polish Header docs #507

--- docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
+++ docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
@@ -1,9 +1,8 @@
 import * as components from '@undataforum/components';
 import * as themeUi from 'theme-ui';
-import preset from '@undataforum/preset';
 
 import DummyLogo from '../../components/dummy-logo';
 
-const scope = { ...components, ...themeUi, DummyLogo, preset };
+const scope = { ...components, ...themeUi, DummyLogo };
 
 export default scope;
