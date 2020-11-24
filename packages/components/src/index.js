38ad3e4d17aacc08c15385dff7bb3874f2bbb573
Thilo Maier <git@maier.asia>
2020-11-24 04:05:52 +0000

    Add Tags component Fixes #638

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,8 +1,10 @@
 export { default as Avatars } from './avatars';
 export { default as Badges } from './badges';
 export { default as Card } from './card';
+export { default as EmailIcon } from './email-icon';
 export { default as EventPreview } from './event-preview';
 export { default as Footer } from './footer';
+export { default as GitHubIcon } from './github-icon';
 export { default as Header } from './header';
 export { default as Icon } from './icon';
 export { default as Names } from './names';
@@ -11,6 +13,5 @@ export { default as PostPreview } from './post-preview';
 export { default as ProfilePreview } from './profile-preview';
 export { default as SocialIcons } from './social-icons';
 export { default as Video } from './video';
-export { default as EmailIcon } from './email-icon';
-export { default as GitHubIcon } from './github-icon';
+export { default as Tags } from './tags';
 export { default as TwitterIcon } from './twitter-icon';
