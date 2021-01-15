adbe283e3fd17e122a85349d2adc9a6830a286b2
Thilo Maier <git@maier.asia>
2021-01-15 21:13:58 +0000

    Improve lint-staged config Fixes #685

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,26 @@
-module.exports = {
-  '*.js': ['eslint --fix', 'prettier --write', 'prettier --check'],
-  '*.{md,mdx,css,yaml,yml}': ['prettier --write', 'prettier --check'],
+const micromatch = require('micromatch');
+const prettier = require('prettier');
+
+// https://coding.maier.tech/chunks/optimizing-lint-staged-config-js-for-prettier/
+
+const prettierSupportedExtensions = prettier
+  .getSupportInfo()
+  .languages.map(({ extensions }) => extensions)
+  .flat();
+
+const addQuotes = (a) => `"${a}"`;
+
+module.exports = (allStagedFiles) => {
+  const eslintFiles = micromatch(allStagedFiles, ['**/*.js'], { dot: true });
+  const prettierFiles = micromatch(
+    allStagedFiles,
+    prettierSupportedExtensions.map((extension) => `**/*${extension}`),
+    { dot: true }
+  );
+  const linters = [];
+  if (eslintFiles.length > 0)
+    linters.push(`eslint --fix ${eslintFiles.join(' ')}`);
+  if (prettierFiles.length > 0)
+    linters.push(`prettier --write ${prettierFiles.map(addQuotes).join(' ')}`);
+  return linters;
 };
